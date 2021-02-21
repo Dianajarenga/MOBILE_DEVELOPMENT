8c3823a87430e10e9c11bbc58fb4621319192b41
Dianajarenga <79199013+Dianajarenga@users.noreply.github.com>
2021-02-21 13:28:21 +0300

    Add files via upload

---
+++ kotlinassignment.kt
@@ -0,0 +1,28 @@
+
+
+
+fun main() {
+    println("STUDENT RECORDS")
+    val name = "Diana Jarenga"
+    println(name)
+    val Age = "Age :  "
+    print(Age)
+    var Int = 20
+    Int = 21
+    println(Int)
+    val phoneNumber = "Phone Number"
+    print(phoneNumber)
+    var number = " : +254 713 535 7064"
+    println(number)
+    var weight = "Weight  :"
+    print(weight)
+    var int = " 56"
+    print(int)
+    val nam = " Kg"
+    println(nam)
+    var Citizenship="Citizenship : kenyan"
+    println(Citizenship)
+    val kenyanCitizenship:Boolean= true
+    println(kenyanCitizenship)
+
+}
