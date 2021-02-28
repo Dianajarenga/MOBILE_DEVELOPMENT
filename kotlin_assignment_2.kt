3539e9da84d65e49cd65981f4fc9eb78bf78d12f
Dianajarenga <79199013+Dianajarenga@users.noreply.github.com>
2021-02-28 13:02:57 +0300

    Add files via upload

---
+++ kotlin_assignment_2.kt
@@ -0,0 +1,45 @@
+
+
+
+
+fun main() {
+
+    name()
+    var result=modulus(150,9)
+    println(result)
+    add()
+    string()
+
+}
+fun name (){
+    val name="Hello Diana "
+    println(name)
+}fun num(){
+    var num1=150
+    var num2=9
+
+
+}
+
+fun modulus(num1:Int,num2:Int):Int{
+    var modulus=num1%num2
+    return modulus
+}
+fun add (){
+    var num3=50
+    var num4=3
+    var num5=4
+    var num6=500
+
+
+    var sum=num3+num4+num5+num6
+    println(sum)
+}
+
+fun string(){
+    var interestingThing=("I enjoy learning Kotlin")
+    print(interestingThing)
+
+
+
+}
