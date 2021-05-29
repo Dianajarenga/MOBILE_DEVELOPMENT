87b590ce9861eda2d039da3aba723a2ea46eb186
Dianajarenga <79199013+Dianajarenga@users.noreply.github.com>
2021-05-29 09:58:47 +0300

    Add files via upload

--- assesment.kt
+++ assesment.kt
@@ -6,13 +6,14 @@ booklist()
     customer.deposit(50000.00)
    print(customer)
     customer.withdraw(40000.00)
+
+    var save=SavingsAccount(234,"Diana",20000000.00,4)
 }
 data class Book(var title:String,var author:String, var pages:Int)
-fun booklist():Book{
+fun booklist(){
     var book= mutableListOf(Book("Life","Jarenga",100),Book("style","Diana",200))
-   // book.reversed()
+   print(book.reverse())
 
-return booklist()
 }
 class CurrentAccount(var accountNumber:Int,var accountName:String, var balance:Double,) {
     fun deposit(amount: Double): Double {
@@ -34,6 +35,32 @@ class CurrentAccount(var accountNumber:Int,var accountName:String, var balance:D
 }
 
 class SavingsAccount( var accountNumber:Int,var accountName:String, var balance:Double ,var withdrawals:Int){
-    
 
+    fun deposit(amount: Double): Double {
+
+        var amount = balance + amount
+        return amount
+        print(amount)
+    }
+
+    fun withdraw(amoun: Double): Double {
+        var amoun = balance - amoun
+        return amoun
+        print(amoun)
+    }
+
+    fun details() {
+        print("Account $accountNumber with balance $balance is operated by $accountName" )
+    }
 }
+
+
+
+
+    fun withdraw(withdrawals: Int){
+        if (withdrawals<=4){
+            print(withdrawals)
+        }
+        else{print("withrawal failed")}
+    }
+
